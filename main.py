ace12691114409872a356c427cb82a67de143cfc
Nachtalb <na@nachtalb.io>
2023-02-03 23:30:38 +0100

    add 20min

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ async def brack_data(raw):
             + "\n"
             + html.find(class_="product-description__list").text.strip()
         ),
-        "image": html.find(class_="product-tabs__img").href,
+        "image": html.find(class_="product-tabs__img").src,
         "price-before": re.sub(r"\D", "", html.find(class_="js-old-price").text),
         "price-after": re.sub(r"\D", "", html.find(class_="js-deal-price").text),
         "quantity-total": -1,
@@ -153,11 +153,42 @@ async def brack_data(raw):
     yield info
 
 
+async def twenty_min(session):
+    async with session.get("https://myshop.20min.ch/en_US/") as response:
+        return twenty_min_data(await response.text())
+
+
+async def twenty_min_data(raw):
+    html = BeautifulSoup(raw, "html.parser")
+
+    info = {
+        "name": html.find(class_="deal-title").text,
+        "brand": "",
+        "id": -1,
+        "rating": -1,
+        "rating-top": -1,
+        "description": html.find(class_="deal-subtitle").text.strip(),
+        "image": html.find(class_="deal-img").find("img").attrs["data-src"],
+        "price-before": float(html.find(class_="deal-old-price").find("span").text),
+        "price-after": float(html.find(class_="deal-price").text),
+        "quantity-total": int(re.sub(r"\D", "", html.find(class_="deal-products-quantity").text)),
+        "percent-available": int(html.find(class_="deal-inventory").text),
+        "valid-from": datetime.combine(date.today(), datetime.min.time()),
+        "valid-for": timedelta(days=1),
+        "portal": "20min",
+        "url": "https://myshop.20min.ch" + html.find(class_="deal-link").attrs["href"],
+        "currency": "CHF",
+    }
+    info["quantity-sold"] = info["quantity-total"] * (info["percent-available"] / 100)
+    yield info
+
+
 async def main():
     async with ClientSession() as session:
         tasks = [
             digitec(session),
             brack(session),
+            twenty_min(session),
         ]
 
         for result in as_completed(tasks):
